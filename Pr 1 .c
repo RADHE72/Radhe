cc4636d0123bce6c85acbe7e21628be1c9243a8b
RADHE72 <rutvikthakor910@gmail.com>
2024-09-10 21:22:38 +0530

    Create Pr 1 .c

---
+++ Pr 1 .c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main() {
+    int sum = 0;
+
+    // Calculate sum from 1 to 100
+    for(int i = 1; i <= 100; i++) {
+        sum += i;
+    }
+
+    // Display the result
+    printf("The sum of numbers from 1 to 100 is: %d
